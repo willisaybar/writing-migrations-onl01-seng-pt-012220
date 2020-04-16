5afb3c90ca6d3b5adc6701a0a4d4c42b4ef4eb78
Willis Aybar <willisaybar@gmail.com>
2020-04-16 00:22:03 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,11 +2,10 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
     add_column :students do |t|
-      t.integer :grade
-    add_column :students do |t|
-      t.string :birthdate
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string 
     end
-  end
+    end
   end
 
 end
